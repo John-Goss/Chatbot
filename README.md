360bb5ce4382a5a0fcfbec9a4bdc19ee9fc889c2
John <Admin@MacBook-Pro-de-Jonathan.local>
2016-12-26 17:16:17 +0100

    First Commit, some issues to fix (First call has no location)

---
+++ README.md
@@ -0,0 +1,9 @@
+# Weather_Chatbot
+
+/*
+**	Chatbot use the Wit.ai && OpenweatherMap API
+*/
+
+Run: node index.js
+
+Say: Hi Bot, what's the weather in [your_city] ?
---
+++ index.js
@@ -0,0 +1,77 @@
+// @flow
+
+'use strict'
+
+let config = require('./config.js')
+let Wit = require('node-wit').Wit
+let interactive = require('node-wit').interactive
+let req = require('request')
+
+// Send request to Wit AI
+
+const accessToken = config.WIT_TOKEN
+
+const firstEntityValue = (entities, entity) => {
+  const val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][0].value
+  if (!val) {
+    return null
+  }
+  return typeof val === 'object' ? val.value : val
+}
+
+const actions = {
+  send (request, response) {
+    const {sessionId, context, entities} = request
+    const {text, quickreplies} = response
+    console.log('\n' + JSON.stringify(response).replace('{"text":"', '').replace('"}', '') + '\n')
+  },
+  getForecast ({context, entities}) {
+    var location = firstEntityValue(entities, 'location')
+    if (location) {
+      openweathermap(location, function (success, previsions) {
+        if (!success) {
+          console.log('An error occured when called openWeatherAPI, please try again.')
+        }
+        previsions ? context.forecast = ('There is the weather for : ' + previsions.city + ' Temperature : ' + previsions.temperature + '°C Moisture : ' + previsions.humidity + '% Wind : ' + previsions.wind + 'km/h')
+        : context.forecast = 'Error'
+      })
+      context.missingLocation = false
+    } else {
+      console.log('There is no location')
+      context.missingLocation = true
+      delete context.forecast
+    }
+    return context
+  }
+}
+
+// API openWeatherMap Function - Get the weather from API
+
+var openweathermap = function (city, callback) {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&lang=us&units=metric&appid=' + config.OPENWEATHER_TOKEN
+
+  req(url, function (err, response, body) {
+    try {
+      var result = JSON.parse(body)
+      if (result.cod !== 200) {
+        callback(false)
+      } else {
+        var previsions = {
+          temperature: Math.round(result.main.temp),
+          humidity: result.main.humidity,
+          wind: Math.round(result.wind.speed * 3.6),
+          city: result.name
+        }
+        callback(true, previsions)
+      }
+    } catch (e) {
+      callback(false)
+    }
+  })
+}
+
+const client = new Wit({accessToken, actions})
+interactive(client)
